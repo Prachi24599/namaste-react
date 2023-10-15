5a6e0e1f94d021f073604ab8ba06c96f38adccce
Prachi24599 <prachi24599@gmail.com>
2023-10-15 22:34:47 +0530

    html multile nested structure using react

--- App.js
+++ App.js
@@ -11,19 +11,25 @@
  *      <h1>This is h1 tag</h1>
  *      <h2>This is h2 tag</h2>
  *   </div>
+ *  <div id="child2">
+ *      <h1>This is h1 tag</h1>
+ *      <h2>This is h2 tag</h2>
+ *   </div>
  * </div>
  *
  * ReactElement(Object) => HTML(Browser Understands)
  */
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
+const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "This is h1 tag"),
     React.createElement("h2", {}, "This is h2 tag"),
-  ])
-);
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "This is h1 tag"),
+    React.createElement("h2", {}, "This is h2 tag"),
+  ]),
+]);
 
 console.log(parent);
 
