1b8807a828293fb01e2cfd702451f6af3ea97d1c
Prachi Polakhare <prachi24599@gmail.com>
2023-11-28 23:39:40 +0530

    Nested structure

--- App.js
+++ App.js
@@ -4,6 +4,10 @@
  *       <h1>I'm h1 tag</h1>
  *       <h2>I'm h2 tag</h2>
  *    </div>
+ *   <div id="child2">
+ *       <h1>I'm h1 tag</h1>
+ *       <h2>I'm h2 tag</h2>
+ *    </div>
  * </div>
  * 
  * React Element is JavaScript object =>(render)=> Converted into HTML element that browser understands
@@ -14,11 +18,15 @@
 const parent = React.createElement(
   "div", 
   {id: "parent"}, 
-  React.createElement(
+  [ React.createElement(
     "div", 
     {id : "child"},
-    [React.createElement("h1", {}, "I'm h1 tag"), React.createElement("h2", {}, "I'm h2 tag")]
-    )
+    [React.createElement("h1", {}, "I'm h1 tag"), React.createElement("h2", {}, "I'm h2 tag")]), 
+    React.createElement(
+      "div", 
+      {id : "child"},
+      [React.createElement("h1", {}, "I'm h1 tag"), React.createElement("h2", {}, "I'm h2 tag")])
+  ]
 )
 
 // const heading = React.createElement("h1", {id : "heading", xyz : "abc"}, "Hello World from React!");
