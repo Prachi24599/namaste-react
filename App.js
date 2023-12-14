bff72880abb235ee80d73e7371b02e2bad8e9b59
Prachi Polakhare <prachi24599@gmail.com>
2023-12-14 23:39:57 +0530

    creating react element using JSX

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 //React.createElement => Object => HTMLElement(render)
 
 const heading = React.createElement("h1", {}, "Namaste React 🚀");
+console.log(heading);
+
+// JSX is not HTML inside javascript
+// JSX is HTML-like or XML-like syntax
+
+const jsxHeading = <h1 id="heading">Namaste React using JSX! 🚀</h1>
+console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-console.log(heading);
-
-root.render(heading);
+root.render(jsxHeading);
