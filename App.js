298ef48ce9038a7f9632d12416adabf4b7c69cb1
Prachi Polakhare <prachi24599@gmail.com>
2023-12-15 23:36:47 +0530

    component composition

--- App.js
+++ App.js
@@ -1,29 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React Element
-const heading = (<h1 className="heading" tabIndex="1">
+const Title = () => (<h1 className="heading" tabIndex="1">
     Namaste React using JSX! 🚀
     </h1>);
 
-//React Functional Component - Normal JavaScript function that returns JSX code.
-const HeadingComponent = () => {
-    return <h1 className="heading">Namaste React Functional Component</h1>
-}
-
-//IF there is only one line, then we can skip return 
-const HeadingComponent2 = () =>  <h1 className="heading">Namaste React Functional Component</h1>
-
-//If we need to write JSX on multiple lines then we need to wrap it in curly braces
-//This is common convention in industry
-const HeadingComponent3 = () => (
+//Component Composition - Rendering one component inside another
+const HeadingComponent = () => (
     <div id="container">
+        <Title/>
         <h1 className="heading">Namaste React Functional Component</h1>
     </div>
 )
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// root.render(heading);
-//rendering functional component on the page
-root.render(<HeadingComponent3/>);
+root.render(<HeadingComponent/>);
