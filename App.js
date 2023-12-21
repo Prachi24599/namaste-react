25f1848992d0d80a0cb223404708238a86e116fb
Prachi Polakhare <prachi24599@gmail.com>
2023-12-21 23:06:15 +0530

    displaying react element inside another

--- App.js
+++ App.js
@@ -3,18 +3,18 @@ import ReactDOM from "react-dom/client";
 
 //JSX => React.createElement => JS Object => HTMLElement(render)
 
-const Title = () => (<h1 id="heading">
+const element = <span>Just Element</span>
+
+const title = (<h1 id="heading">
+    {element}
     Namaste React using JSX 🚀
     </h1>);
-
-
-const number = 10000;
 
 // Component Composition
 const HeadingComponent = () => (
     <div id="container">
-       <h2> {100 + 300}</h2> 
-        <h1 c>Namaste React Functional Component</h1>
+        {title}
+        <h1>Namaste React Functional Component</h1>
     </div>
 )
 
