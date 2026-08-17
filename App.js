5cd86175151bf89fdb7dc3dae6b5cb7be9911b22
prachi24599 <prachi24599@gmail.com>
2026-08-17 15:35:48 +0200

    React using cdn links

--- App.js
+++ App.js
@@ -1,93 +1,29 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-/**
- * Header
- *  - Logo
- *  - Nav Items
- * Body
- *  - Search 
- *  - Restaurant Container
- *      - Restaurant Card
- *          - Img
- *          - Name of Res, Star Rating, Cuisine, Delivery Time
- * Footer
- *  - Copyright
- *  - Links
- *  - Address
- *  - Contact
- */
+/*
+<div id="parent">
+    <div id="child">
+        <h1>I am h1 tag</h1>
+        <h2>I'm h2 tag in first child</h2>    
+    </div>
+    <div id="child2">
+        <h1>I am h1 tag</h1>    
+        <h2>I'm h2 tag in second child</h2>
+    </div>
+</div>
 
+*/
 
-/**
- * Config Driven UI - Which means controlling your UI using data and the config comes from backend.
- */
+const parent = React.createElement("div", {id : "parent"},
+    [React.createElement("div", {id : "child"}, 
+        [React.createElement("h1", {}, "I am h1 tag"),
+        React.createElement("h2", {}, "I'm h2 tag in the first child")]
+    ),
+    React.createElement("div", {id : "child2"}, 
+        [React.createElement("h1", {}, "I'm also an h1 tag"),
+        React.createElement("h2", {}, "I'm h2 tag in second child")]
+    )]
+)
 
-const Header = () => {
-    return (
-        <div className="header">
-            <div className="logo-container">
-                <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?size=2"/>
-            </div>
-            <div className="nav-items">
-                <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
-                    <li>Cart</li>
-                </ul>
-            </div>
-        </div>
-    )
-}
-
-const RestaurantCard = (props) => {
-    console.log(props);
-    const { resData } = props?.data;
-    console.log(resData);
-    return (
-        <div className="restaurant-card" style={{backgroundColor : "#f0f0f0"}}>
-            <img className="restaurant-logo" alt="restaurant-logo" src="https://b.zmtcdn.com/data/pictures/3/20729743/bae5332c13de2e1df091fee03556efe4_o2_featured_v2.jpg?output-format=webp"/>
-            <h3>{resData?.data?.restaurantName}</h3>
-            <h4>{resData?.data?.cuisine}</h4>
-            <h4>{resData?.data?.rating} stars</h4>
-            <h4>{resData?.data?.deliveryTime} minutes</h4>
-        </div>
-    );
-}
-
-const resObj = {
-    data : {
-        restaurantName : "KFC",
-        cuisine : "Burger, Fast Food",
-        rating : 4.4,
-        deliveryTime : 38
-    }
-}
-
-const Body = () => {
-    return (
-        <div className="body">
-            <div className="search-container">
-                <input type="text"/>
-                <button>Search</button>
-            </div>
-            <div className="restaurant-container">
-                {/* <RestaurantCard resName="Meghana Foods" cuisine="Biryani, North Indian"/> */}
-                <RestaurantCard resObj={resObj} />
-                {/* <RestaurantCard resName="KFC" cuisine="Burger, Fast Food" /> */}
-            </div>
-        </div>
-    )
-}
-
-const AppLayout = () => {
-    return (
-        <div className="app">
-            <Header/>
-            <Body/>
-        </div>
-    )
-}
-
+// const heading = React.createElement("h1", {id : "heading"}, "Hello world from React");
+console.log(parent); 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout/>)
+root.render(parent);
